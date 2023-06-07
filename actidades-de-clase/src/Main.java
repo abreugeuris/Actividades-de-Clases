78e6f537ef8d60fb95e9be461427da12352197a2
abreugeuris <geurisabreu08@gmail.com>
2023-06-07 15:03:31 -0400

    Ejercicio 5 adivina numero Aleatorio

--- actidades-de-clase/src/Main.java
+++ actidades-de-clase/src/Main.java
@@ -1,10 +1,12 @@
 import clase_5.*;
 
+import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
 
-       /*Ejercicio1 ejercicio1 = new Ejercicio1();
+        Ejercicio1 ejercicio1 = new Ejercicio1();
         System.out.println("---Ejercicio-1 Calculadora de edad de perros.---");
         System.out.println(ejercicio1.CalculaEdadPerro());
 
@@ -13,12 +15,27 @@ public class Main {
         System.out.println(ejercicio2.conversorMillasKilometros());
 
         System.out.println("--- Ejercicio-3  Calculadora de descuento---");
-        Ejecicio3 ejecicio3 = new Ejecicio3();
-        System.out.println( ejecicio3.CalculadoraDescuento());*/
+        Ejercicio3 ejecicio3 = new Ejercicio3();
+        System.out.println( ejecicio3.CalculadoraDescuento());
 
         System.out.println("--- Ejercicio-4  Calculadora de Propinas---");
-        Ejecicio4 ejecicio4 = new Ejecicio4();
+        Ejercicio4 ejecicio4 = new Ejercicio4();
         System.out.println( ejecicio4.CalculadoraPropinas());
+
+        System.out.println("--- Ejercicio-5 Juego de adivinar el número ---");
+        Ejercicio5 ejecicio5 = new Ejercicio5();
+
+        int numeroAleatorio = ejecicio5.generaNumeroAleatorio();
+        Scanner entradaDatos = new Scanner(System.in);
+        System.out.println("Introduzca un numero en el rango 0-100 : ");
+        int numero = entradaDatos.nextInt();
+
+        if (numero == numeroAleatorio) {
+            System.out.println("haz adivinado el nuemro, Felicitaciones!!!!");
+        } else {
+            System.out.println("Lo siento, Intentalo nuevamente!");
+        }
+
 
     }
 }
---
+++ actidades-de-clase/src/clase_5/Ejercicio5.java
@@ -0,0 +1,10 @@
+package clase_5;
+
+import java.util.Random;
+
+public class Ejercicio5 {
+
+    public int generaNumeroAleatorio(){
+        return (int) (Math.random() *100.0);
+    }
+}
