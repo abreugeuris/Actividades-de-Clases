5bd7e3fd8606c10cc288412ea84d7e56993914dc
abreugeuris <geurisabreu08@gmail.com>
2023-06-06 20:14:10 -0400

    Ejercicio-1 y Ejercicio-2

---
+++ Actividades-de-Clases/src/Main.java
@@ -0,0 +1,16 @@
+import clase_5.Ejercicio1;
+import clase_5.Ejercicio2;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Ejercicio1  ejercicio1 = new Ejercicio1();
+        System.out.println("Ejercicio-1");
+        System.out.println(ejercicio1.CalculaEdadPerro());
+
+        System.out.println("Ejercicio-2");
+        Ejercicio2 ejercicio2 = new Ejercicio2();
+        System.out.println(ejercicio2.conversorMillasKilometros());
+
+    }
+}
