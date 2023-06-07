3773b93ca910b5d5ad4ca32a50ffa5f82cee9dad
abreugeuris <geurisabreu08@gmail.com>
2023-06-07 10:04:59 -0400

    Cracion de ejercicios 3 y 4

--- actidades-de-clase/src/Main.java
+++ actidades-de-clase/src/Main.java
@@ -4,13 +4,21 @@ import clase_5.*;
 public class Main {
     public static void main(String[] args) {
 
-        Ejercicio1 ejercicio1 = new Ejercicio1();
-        System.out.println("Ejercicio-1");
+       /*Ejercicio1 ejercicio1 = new Ejercicio1();
+        System.out.println("---Ejercicio-1 Calculadora de edad de perros.---");
         System.out.println(ejercicio1.CalculaEdadPerro());
 
-        System.out.println("Ejercicio-2");
+        System.out.println("---Ejercicio-2 Conversor de millas a kilómetros---");
         Ejercicio2 ejercicio2 = new Ejercicio2();
         System.out.println(ejercicio2.conversorMillasKilometros());
+
+        System.out.println("--- Ejercicio-3  Calculadora de descuento---");
+        Ejecicio3 ejecicio3 = new Ejecicio3();
+        System.out.println( ejecicio3.CalculadoraDescuento());*/
+
+        System.out.println("--- Ejercicio-4  Calculadora de Propinas---");
+        Ejecicio4 ejecicio4 = new Ejecicio4();
+        System.out.println( ejecicio4.CalculadoraPropinas());
 
     }
 }
